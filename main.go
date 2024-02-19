1a5ca0655867e3f6d0695a119dbe115874d5fa85
olhamish <olgami1402@gmail.com>
2024-02-19 22:08:10 +0200

    finish lab task

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
-  "time"
+	"encoding/json"
+	"net/http"
+	"time"
 )
+
+func main() {
+	http.HandleFunc("/time", getTimeHandler)
+	err := http.ListenAndServe(":8795", nil)
+	if err != nil {
+		return
+	}
+}
+
+func getTimeHandler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format(time.RFC3339)
+	response := map[string]string{"time": currentTime}
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		return
+	}
+}
